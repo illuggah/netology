b4e32fc90d88a9264fdf787ceb8eeb06e4bbdaee
illuggah <illuggah@mail.ru>
2017-08-08 21:11:33 +0700

    Create php-lesson2.php

---
+++ php-lesson2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Числа Фибоначчи</title>
+</head>
+<body>
+	<?php
+		$x = $_GET[x];
+		echo 'Initial number: ' . $x . '<br>';
+		$first = 1;
+		$second = 1;
+		while ($i = 1) {
+			if ($first == $x) {
+				echo 'Задуманное число входит в числовой ряд';
+				break;
+			} elseif ($x > $first) {
+				$third = $first;
+				$first = $first + $second;
+				$second = $third;
+			} elseif ($x < $first) {
+				echo 'Задуманное число НЕ входит в числовой ряд';
+				break;
+			}
+		};
+	?>
+</body>
+</html>
